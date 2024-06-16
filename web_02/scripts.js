47ceb41825641c2798e276159f53c10457aed5c6
Yeonsu1027 <rito1205@naver.com>
2024-06-16 21:55:40 +0900

    web

--- web_02/scripts.js
+++ web_02/scripts.js
@@ -1,11 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const nav = document.querySelector('nav');
-    const toggle = document.createElement('div');
-    toggle.innerHTML = '&#9776;';
-    toggle.className = 'nav-toggle';
-    document.body.insertBefore(toggle, nav);
+document.addEventListener("DOMContentLoaded", ()=>{
+    const modal = document.getElementById("modal");
+    const modalImg = document.getElementById("modalImage");
+    const captionText = document.getElementById("caption");
+    const closeModal = document.getElementsByClassName("close")[0];
+    
+    const imgBoxes = document.querySelectorAll(".img_box2");
+    imgBoxes.forEach(imgBox => {
+        imgBox.addEventListener("click", function() {
+            modal.style.display = "block";
+            modalImg.src = this.querySelector("img").src;
+            captionText.innerHTML = this.querySelector("img").alt;
+        });
+    });
 
-    toggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
+    closeModal.addEventListener("click", function() {
+        modal.style.display = "none";
     });
 });
