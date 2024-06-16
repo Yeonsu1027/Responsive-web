f89d1a26c50e4e5786994fd284c1885035b6c734
Yeonsu1027 <rito1205@naver.com>
2024-06-17 01:26:19 +0900

    반응형

--- web_02/scripts.js
+++ web_02/scripts.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+
+    
+
+    // 사진 자세히보기
     const modal = document.getElementById("modal");
     const modalImg = document.getElementById("modalImage");
     const captionText = document.getElementById("caption");
@@ -16,4 +20,16 @@ document.addEventListener("DOMContentLoaded", ()=>{
     closeModal.addEventListener("click", function() {
         modal.style.display = "none";
     });
+
+    // 메뉴 클릭 유지용
+    const menuItems = document.querySelectorAll(".menu li");
+    menuItems.forEach(item => {
+        item.addEventListener("click", function() {
+            // 모든 li에서 click 클래스 제거
+            menuItems.forEach(li => li.classList.remove('click'));
+            // 현재 클릭한 li에 click 클래스 추가
+            this.classList.add('click');
+        });
+    });
+    
 });
